da0580f074e368b3c7915cb8a2cfd7475e9e4226
KanHar <knaan.harpaz@gmail.com>
2023-05-09 15:21:48 +0300

    Working openAI API

---
+++ src/completions.ts
@@ -0,0 +1,16 @@
+import {Configuration, OpenAIApi} from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function main() {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Hello world",
+  });
+  console.log(completion.data.choices[0].text);
+}
+
+main();
