a10dccfb345ec6185fc09c0ef4a03415aa81a47e
KanHar <knaan.harpaz@gmail.com>
2023-05-09 17:05:22 +0300

    Add sample and saved embeddings

--- src/create_embeddings.ts
+++ src/create_embeddings.ts
@@ -8,51 +8,55 @@ const readFileAsync = promisify(fs.readFile);
 const writeFileAsync = promisify(fs.writeFile);
 const mkdirAsync = promisify(fs.mkdir);
 
-async function processFile(filePath: string): Promise<void> {
-  try {
-    // Read file contents
-    const fileContent = await readFileAsync(filePath, "utf-8");
+async function getFileEmbedding(filePath: string, verbose: boolean = false): Promise<{
+  original_file_name: string;
+  data: Array<{ paragraph: string; embedding: Array<number> }>;
+}> {
+  // Read file contents
+  const fileContent = await readFileAsync(filePath, "utf-8");
 
-    // Create hash of the file content
-    const hash = crypto.createHash("md5").update(fileContent).digest("hex");
+  // Create hash of the file content
+  const hash = crypto.createHash("md5").update(fileContent).digest("hex");
 
-    // Check if the saved_embeddings folder exists, if not, create it
-    if (!fs.existsSync("saved_embeddings")) {
-      await mkdirAsync("saved_embeddings");
-    }
-
-    // Check if the JSON file with the same MD5 hash already exists
-    const jsonFilePath = `saved_embeddings/${hash}.json`;
-    if (fs.existsSync(jsonFilePath)) {
-      console.log("Embeddings already exist. Skipping.");
-      return;
-    }
-
-    // Split file into paragraphs
-    const paragraphs = fileContent
-      .split("\n")
-      .map((s) => s.trim())
-      .filter((s) => s.length > 0);
-
-    // Get embedding vectors
-    const embeddings = await Promise.all(paragraphs.map(get_embedding_vector));
-
-    // Create the output JSON
-    const outputJSON = {
-      original_file_name: path.basename(filePath),
-      data: paragraphs.map((paragraph, index) => ({
-        paragraph: paragraph,
-        embedding: embeddings[index],
-      })),
-    };
-
-    // Save the JSON to the saved_embeddings folder
-    await writeFileAsync(jsonFilePath, JSON.stringify(outputJSON));
-
-    console.log("Embeddings saved successfully.");
-  } catch (error) {
-    console.error("Error processing file:", error);
+  // Check if the saved_embeddings folder exists, if not, create it
+  if (!fs.existsSync("saved_embeddings")) {
+    await mkdirAsync("saved_embeddings");
   }
+
+  // Check if the JSON file with the same MD5 hash already exists
+  const jsonFilePath = `saved_embeddings/${hash}.json`;
+  if (fs.existsSync(jsonFilePath)) {
+    if (verbose) {
+      console.log("Embeddings already exist. Skipping.");
+    }
+    return;
+  }
+
+  // Split file into paragraphs
+  const paragraphs = fileContent
+    .split("\n")
+    .map((s) => s.trim())
+    .filter((s) => s.length > 0);
+
+  // Get embedding vectors
+  const embeddings = await Promise.all(paragraphs.map(get_embedding_vector));
+
+  // Create the output JSON
+  const outputJSON = {
+    original_file_name: path.basename(filePath),
+    data: paragraphs.map((paragraph, index) => ({
+      paragraph: paragraph,
+      embedding: embeddings[index],
+    })),
+  };
+
+  // Save the JSON to the saved_embeddings folder
+  await writeFileAsync(jsonFilePath, JSON.stringify(outputJSON));
+
+  if (verbose) {
+  console.log("Embeddings saved successfully.");
+  }
+  return outputJSON;
 }
 
 async function main() {
@@ -62,7 +66,7 @@ async function main() {
   }
 
   const inputFilePath = process.argv[2];
-  await processFile(inputFilePath);
+  await getFileEmbedding(inputFilePath, true);
 }
 
 main();
